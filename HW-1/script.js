5e349c83376c9c6ac023ff7ff6005d58fb5fad55
HarryPotter-4000 <80061297+HarryPotter-4000@users.noreply.github.com>
2021-09-23 09:57:25 +0300

    fixed

--- HW-1/script.js
+++ HW-1/script.js
@@ -9,9 +9,7 @@ const basket = bread + butter + cheese;
 
 const integerAmount = Math.floor(bread) + Math.floor(butter) + Math.floor(cheese);
 const roundToHundreds = Math.round(integerAmount / 100) * 100;
-function isEvenNumber() {
-    return integerAmount % 2 === 0;
-};
+const isEvenNumber = integerAmount % 2 === 0;
 const change = 500 - basket;
 const avg = parseInt((basket / 3).toFixed(2), 10);
 const discount = Math.floor(Math.random() * 101);
@@ -29,7 +27,7 @@ div.innerHTML =
         <li>Сума товарів: ${basket} гривень;</li>
         <li>Сума цілої частини вартості : ${integerAmount} гривень;</li>
         <li>Сума товарів округлена до сотень: ${roundToHundreds} гривень;</li>
-        <li>Парне чи непарне число: ${isEvenNumber()};</li>
+        <li>Парне чи непарне число: ${isEvenNumber};</li>
         <li>Сума решти з 500гр : ${change} гривень;</li>
         <li>Середнє значення цін: ${avg} гривень;</li>
         <li>Випадкова знижка: ${discount}%;</li>
