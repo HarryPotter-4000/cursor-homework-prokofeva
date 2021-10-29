a405ee31cb465385e2a513b0de254d95913a3f98
HarryPotter-4000 <80061297+HarryPotter-4000@users.noreply.github.com>
2021-10-29 19:59:39 +0300

    hw-12

---
+++ HW-12/script.js
@@ -0,0 +1,20 @@
+function playSoundBykey(key) {
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
+  const keyElement = document.querySelector(`.key[data-key="${key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  keyElement.classList.add('playing');
+}
+const keys = document.querySelectorAll('.keys>div[data-key]');
+keys.forEach(key => {
+  key.addEventListener('click', event => {
+    playSoundBykey(event.currentTarget.dataset.key);
+  })
+  key.addEventListener('transitionend', event => {
+    event.currentTarget.classList.remove('playing');
+  })
+})
+window.addEventListener('keydown', (event) => {
+  playSoundBykey(event.keyCode);
+});
